63c34aeb1a3bef10fc3793772b0eff580d2e1c8c
M Hammad Afzal <110812807+MHammadAfzal@users.noreply.github.com>
2022-10-15 22:08:26 +0500

    Modified_Some_Errors

--- Assignment No 1.cpp
+++ Assignment No 1.cpp
@@ -1,135 +1,120 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 
-struct Node{
+struct node{
 	int data;
-	Node *next;
+	struct node *link;
 };
 
-class List {
+class List{
 	private:
 		int size;
-	    Node *head;
-		Node *current;
-		Node *lastcurrent;
+		node *head;
+		node *curr;
+		node *prevCurr;
 	public:
 		List(){
 			size = 0;
-			head=new Node();
-			current=NULL;
-			lastcurrent=NULL;
+			head = new node();
+			curr = NULL;
+			prevCurr = NULL;
 		}
-		
-	void add(int data){
-		Node *newNode = new Node();
-		newNode -> data = data;
-		if(current != NULL){
-				newNode -> next = current -> next;
-				current -> next = newNode;
-				lastcurrent = current;
-				current = current -> next; 
-			}
-		else{
-				newNode -> next = NULL;
-				head -> next = newNode;
-				lastcurrent = head;
-				current = newNode;
+		void add(int data){
+			node *newNode = new node();
+			newNode->data = data;
+			
+			if(curr != NULL){
+				newNode->link = curr->link;
+				curr->link = newNode;
+				prevCurr = curr;
+				curr = curr->link;
+			}else{
+				newNode->link = NULL;
+				head->link = newNode;
+				prevCurr = head;
+				curr = newNode;
 			}
 			size++;
 		}
-		
-	void start(){
-			current = head;
-			lastcurrent = head;
-		}
-		
-	bool next(){
-			if(current == NULL){
-				return false;
+		void display(){
+			node *ptr = head->link;
+			while(ptr != NULL){
+				cout<<ptr->data<<" ";
+				ptr = ptr->link;
 			}
-			
-			lastcurrent = current;
-			current = current -> next;
-			if (current == NULL)
-				return false;
-			else 
-				return true;
+			cout<<endl;
 		}
-		
-	int length(){
-			return size;
+		void start(){
+			curr = head;
+			prevCurr = head;
 		}
-		
-	int get(){
-			return current -> data;
-		}
-		
-	void find(int data){
-		Node *iter = head -> next;
-		while(iter != NULL){
-			if(iter -> data == data){
-				cout<<" The "<<data<<" is present in list";
-				cout<<endl;
-				break;
+		void end(){
+			curr = head;
+			while(curr->link->link != NULL){
+				curr = curr->link;
 			}
-			else{
-				iter = iter -> next;
+			prevCurr = curr;
+			curr = curr->link;
+		}
+		void find(int data){
+			node *iter = head->link;
+			while(iter != NULL){
+				if(iter->data == data){
+					cout<<"The "<<data<<" is present";
+					cout<<endl;
+					break; 
+				}else{
+					iter = iter->link;
+				}
 			}
 		}
-	}
-	
-	void remove(){
-		if (current != NULL && current!=head){
-				lastcurrent -> next = current -> next;
-				delete current;
-				current = lastcurrent -> next;
+		void remove(){
+			if(curr != NULL && curr != head){
+				prevCurr->link = curr->link;
+				delete curr;
+				curr = prevCurr->link;
 				size--;
 			}
+			
 		}
-		
-		
-	void update(int data){
-		Node *ptr = head -> next;
-		ptr -> data = data;
-		
-	}
-	
-	void end(){
-		current = head;
-		while(current -> next -> next != NULL){
-			current = current -> next;
+		void update(int data){
+			node *ptr = head->link;
+			ptr->data = data;
 		}
-		lastcurrent = current;
-		current = current -> next;
-	}
-	
-	void display(){
-		Node *ptr = head;
-		while(ptr != NULL){
-			cout<<" "<<ptr -> data<< " ";
-			ptr = ptr -> next;
+		int length(){
+			return size;
 		}
-		cout<<endl;
-	}
-	
-	
-	~List(){
-		current = head -> next;
-		Node *delNode;
-		for(int i=1; i<=size; i++){
-			delNode = current;
-			cout<<" Deleted Node is "<<delNode -> data<<endl;
-			current = current -> next;
-			delete delNode;
+		int get(){
+			return curr->data;
 		}
-		delete head;
-	}
-		
+		bool next(){
+			if(curr == NULL){
+				return false;
+			}
+			prevCurr = curr;
+			curr = curr->link;
+			if (curr == NULL){
+				return false;
+			}else {
+				return true;
+			}	
+		}
+		~List(){
+			curr = head->link;
+			node *delNode;
+			for(int i = 1; i <= size; i++){
+				delNode = curr;
+				cout<<"\nDeleted Node is : "<<delNode->data;
+				curr = curr->link;
+				delete delNode;
+			}
+			delete head;
+		}
 };
 
-
-
 int main(){
+	
 	List L1;
 	List L2;
 	
@@ -138,15 +123,13 @@ int main(){
 	L1.add(3);
 	L1.add(6);
 	L1.add(20);
-	
 	L1.display();
-	
 	L1.start();
-
-	while(L1.next()){
- 	cout<<" LIST ELEMENT IS \t"<<L1.get()<<endl<<" SIZE of LIST IS "<<L1.length()<<endl;
+	while(L1.next())
+	{
+	cout<<" LIST ELEMENT IS \t"<<L1.get()<<" SIZE of LIST IS "<<L1.length()<<endl;
 	}
-	cout<<endl;
+
 	
 	L1.start();
 	L1.find(7);
@@ -157,18 +140,18 @@ int main(){
 	L1.update(60);
 	
 	L1.start();
-	
 	L1.add(100);
 	L1.end();
 	L1.add(200);
 	L1.add(200);
-    L1.start();
-    
-    cout<<endl;
-    while(L1.next()){
-    cout<<" LIST ELEMENT IS \t"<<L1.get()<<endl<<" SIZE of LIST IS "<<L1.length()<<endl;
+	
+	L1.start();
+	while(L1.next())
+	{
+	cout<<" LIST ELEMENT IS \t"<<L1.get()<<" SIZE of LIST IS "<<L1.length()<<endl;
 	}
-	cout<<endl;
+	
+	getch();
 	
 	return 0;
 }
